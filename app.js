6d656ffdd336e68f13809cd60e5b9da0f99741aa
chivicoko <victor.c.okoye@gmail.com>
2023-11-24 17:18:20 +0100

    updated

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 
 let html_1 = `
-<svg width="20" height="20" viewBox="0 0 21 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path fill-rule="evenodd" clip-rule="evenodd" d="M6.21967 8.46967C6.51256 8.17678 6.98744 8.17678 7.28033 8.46967L10.75 11.9393L14.2197 8.46967C14.5126 8.17678 14.9874 8.17678 15.2803 8.46967C15.5732 8.76256 15.5732 9.23744 15.2803 9.53033L11.2803 13.5303C10.9874 13.8232 10.5126 13.8232 10.2197 13.5303L6.21967 9.53033C5.92678 9.23744 5.92678 8.76256 6.21967 8.46967Z" fill="#4A4A4A"/>
-</svg>
+<button>
+    <svg role="button" tabindex="0" aria-label="Search" class="arrow" width="20" height="20" viewBox="0 0 21 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path fill-rule="evenodd" clip-rule="evenodd" d="M6.21967 8.46967C6.51256 8.17678 6.98744 8.17678 7.28033 8.46967L10.75 11.9393L14.2197 8.46967C14.5126 8.17678 14.9874 8.17678 15.2803 8.46967C15.5732 8.76256 15.5732 9.23744 15.2803 9.53033L11.2803 13.5303C10.9874 13.8232 10.5126 13.8232 10.2197 13.5303L6.21967 9.53033C5.92678 9.23744 5.92678 8.76256 6.21967 8.46967Z" fill="#4A4A4A"/>
+    </svg>
+</button>
 `;
 
 let html_2 = `
-<svg width="20" height="20" viewBox="0 0 21 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path fill-rule="evenodd" clip-rule="evenodd" d="M15.0303 12.2803C14.7374 12.5732 14.2626 12.5732 13.9697 12.2803L10.5 8.81066L7.03033 12.2803C6.73744 12.5732 6.26256 12.5732 5.96967 12.2803C5.67678 11.9874 5.67678 11.5126 5.96967 11.2197L9.96967 7.21967C10.2626 6.92678 10.7374 6.92678 11.0303 7.21967L15.0303 11.2197C15.3232 11.5126 15.3232 11.9874 15.0303 12.2803Z" fill="#4A4A4A"/>
-</svg>
+<button>
+    <svg class="arrow" width="20" height="20" viewBox="0 0 21 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path fill-rule="evenodd" clip-rule="evenodd" d="M15.0303 12.2803C14.7374 12.5732 14.2626 12.5732 13.9697 12.2803L10.5 8.81066L7.03033 12.2803C6.73744 12.5732 6.26256 12.5732 5.96967 12.2803C5.67678 11.9874 5.67678 11.5126 5.96967 11.2197L9.96967 7.21967C10.2626 6.92678 10.7374 6.92678 11.0303 7.21967L15.0303 11.2197C15.3232 11.5126 15.3232 11.9874 15.0303 12.2803Z" fill="#4A4A4A"/>
+    </svg>
+</button>
 `;
 
 let isTrue = true;
@@ -47,7 +51,15 @@ let svg_2 = `
 </svg>
 `
 
+let svg_3 = `
+<svg class="step-hover" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+  <circle cx="12" cy="12" r="10" stroke="#8A8A8A" stroke-width="2.08333" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+`
+
 const checkBtns = document.querySelectorAll('.check-btns');
+
+
 
 checkBtns.forEach(btn => {
     btn.setAttribute('data-checked', 'false');
@@ -96,13 +108,15 @@ const user = document.querySelector('.user');
 const userModal = document.querySelector('.userModal')
 user.onclick = () => {
     userModal.style.display = userModal.style.display === 'none' ? 'block' : 'none';
+    notificationModal.style.display = 'none';
 }
 
 //   user modal
-const notification = document.querySelector('#bell-svg');
+const notification = document.querySelector('#bell');
 const notificationModal = document.querySelector('.notificationModal')
 notification.onclick = () => {
     notificationModal.style.display = notificationModal.style.display === 'none' ? 'block' : 'none';
+    userModal.style.display = 'none';
 }
 
 
